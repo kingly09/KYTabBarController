c94ce79494ad732a80f43c02a8afdff8f2771043
kingly <libintm@163.com>
2017-12-12 11:37:32 +0800

    KYTabBarControllerExtention

--- libs/UIViewController+KYTabBarControllerExtention.h
+++ libs/UIViewController+KYTabBarControllerExtention.h
@@ -50,9 +50,9 @@ typedef void (^KYPushOrPopCallback)(NSArray<__kindof UIViewController *> *viewCo
 @property (nonatomic, readonly) UIControl *ky_tabButton;
 
 /*!
- * @attention 
-   - 调用该方法前已经添加了系统的角标，调用该方法后，系统的角标并未被移除，只是被隐藏，调用 `-ky_removeTabBadgePoint` 后会重新展示。
-   - 不支持 KYPlusChildViewController 对应的 TabBarItem 角标设置，调用会被忽略。
+ * @attention
+ - 调用该方法前已经添加了系统的角标，调用该方法后，系统的角标并未被移除，只是被隐藏，调用 `-ky_removeTabBadgePoint` 后会重新展示。
+ - 不支持 KYPlusChildViewController 对应的 TabBarItem 角标设置，调用会被忽略。
  */
 - (void)ky_showTabBadgePoint;
 
@@ -81,8 +81,8 @@ typedef void (^KYPushOrPopCallback)(NSArray<__kindof UIViewController *> *viewCo
  @param classType 需要选择的控制器所属的类。
  @return 最终被选择的控制器。
  @attention 注意：
-                - 方法中的参数和返回值都是 `UIViewController` 的子类，但并非 `UINavigationController` 的子类。
-                - 如果 TabBarViewController 的 viewControllers 中包含多个相同的 `classType` 类型，会返回最左端的一个。
+ - 方法中的参数和返回值都是 `UIViewController` 的子类，但并非 `UINavigationController` 的子类。
+ - 如果 TabBarViewController 的 viewControllers 中包含多个相同的 `classType` 类型，会返回最左端的一个。
  
  */
 - (UIViewController *)ky_popSelectTabBarChildViewControllerForClassType:(Class)classType;
@@ -91,31 +91,28 @@ typedef void (^KYPushOrPopCallback)(NSArray<__kindof UIViewController *> *viewCo
  * Pop 到当前 `NavigationController` 的栈底，并改变 `TabBarController` 的 `selectedViewController` 属性，并将被选择的控制器在 `Block` 回调中返回。
  @param classType 需要选择的控制器所属的类。
  @attention 注意：
-                - 方法中的参数和返回值都是 `UIViewController` 的子类，但并非 `UINavigationController` 的子类。
-                - 如果 TabBarViewController 的 viewControllers 中包含多个相同的 `classType` 类型，会返回最左端的一个。
+ - 方法中的参数和返回值都是 `UIViewController` 的子类，但并非 `UINavigationController` 的子类。
+ - 如果 TabBarViewController 的 viewControllers 中包含多个相同的 `classType` 类型，会返回最左端的一个。
  */
 - (void)ky_popSelectTabBarChildViewControllerForClassType:(Class)classType
                                                 completion:(KYPopSelectTabBarChildViewControllerCompletion)completion;
 
-
-/**
- 如果当前的 `NavigationViewController` 栈中包含有准备 Push 到的目标控制器，可以选择 Pop 而非 Push。
- 
- @param viewController viewController Pop 或 Push 到的“目标控制器”，由 completionHandler 的参数控制 Pop 和 Push 的细节。
- @param animated Pop 或 Push 时是否带动画
- @param callback 回调，如果传 nil，将进行 Push。callback 包含以下几个参数：
- *      viewControllers 表示与“目标控制器”相同类型的控制器；
- *      completionHandler 包含以下几个参数
- *         shouldPop 是否 Pop
- *         viewControllerPopTo Pop 回的控制器
- *         shouldPopSelectTabBarChildViewController 在进行 Push 行为之前，是否 Pop 到当前 `NavigationController` 的栈底。
- *                                                                                            可能的值如下：
- *                                                                                        NO 如果上一个参数为 NO，下一个参数 index 将被忽略。
- *                                                                                        YES 会根据 index 参数改变 `TabBarController` 的 `selectedViewController` 属性。
- *                                                                                        注意：该属性在 Pop 行为时不起作用。
- *        index Pop 改变 `TabBarController` 的 `selectedViewController` 属性。注意：该属性在 Pop 行为时不起作用。
- *
- *
+/*!
+ *@brief 如果当前的 `NavigationViewController` 栈中包含有准备 Push 到的目标控制器，可以选择 Pop 而非 Push。
+ *@param viewController Pop 或 Push 到的“目标控制器”，由 completionHandler 的参数控制 Pop 和 Push 的细节。
+ *@param animated Pop 或 Push 时是否带动画
+ *@param callback 回调，如果传 nil，将进行 Push。callback 包含以下几个参数：
+    * * viewControllers 表示与“目标控制器”相同类型的控制器；
+    * * completionHandler 包含以下几个参数：
+    * * shouldPop 是否 Pop
+    * * viewControllerPopTo Pop 回的控制器
+    * * shouldPopSelectTabBarChildViewController 在进行 Push 行为之前，是否 Pop 到当前 `NavigationController` 的栈底。
+ 可能的值如下：
+ NO 如果上一个参数为 NO，下一个参数 index 将被忽略。
+ YES 会根据 index 参数改变 `TabBarController` 的 `selectedViewController` 属性。
+ 注意：该属性在 Pop 行为时不起作用。
+    * * index Pop 改变 `TabBarController` 的 `selectedViewController` 属性。
+ 注意：该属性在 Pop 行为时不起作用。
  */
 - (void)ky_pushOrPopToViewController:(UIViewController *)viewController
                              animated:(BOOL)animated
